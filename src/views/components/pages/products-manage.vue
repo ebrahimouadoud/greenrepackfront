66adc4e05a3dc8e56da960a5fe948ba39b216e45
eouadoud <ebr.ouadoud@gmail.com>
2021-09-28 20:03:30 +0200

    resall management by admin

--- src/views/components/pages/products-manage.vue
+++ src/views/components/pages/products-manage.vue
@@ -38,7 +38,7 @@
                     </div>
                 </div>
                 <b-table
-                    ref="userstab"
+                    ref="productsList"
                     :items="loadData"
                     :busy="isBusy"
                     :fields="fields"
@@ -53,31 +53,103 @@
                     <strong>Chargement...</strong>
                     </div>
                 </template>
-                <template v-slot:cell(created_at)="data">
-                    {{ data.item.created_at | formatDate }}
+                <template v-slot:cell(prixPropose)="data">
+                    {{ data.item.revente.prixPropose + "€" }} 
                 </template>
-                <template v-slot:cell(status)="data">
+                <template v-slot:cell(phase)="data">
                     
-                    <span v-if="data.item.status" class="label label-success">Activé</span>
-                    <span v-else class="label label-danger">Désactivé</span>
+                    <span v-if="data.item.phase=='En Attend' && (data.item.revente.etat=='En Attendant' || data.item.revente.etat=='Accepté')" class="label label-warning">En attente</span>
+                    <span v-else-if="data.item.revente.etat=='Refusé'" class="label label-danger">Revente refusé</span>
+                    <span v-else-if="data.item.phase=='Reçu'" class="label label-success">Reçu</span>
+                    <span v-else-if="data.item.phase=='Renvoyé'" class="label label-danger">Renvoyé</span>
+                    <span v-else-if="data.item.phase=='En vente'" class="label label-info">En vente <span> à {{data.item.prix_vente}} </span> </span> 
+                    <span v-else-if="data.item.phase=='Vendu'" class="label label-inverse">Vendu</span>
                     
                 </template>
                 <template v-slot:cell(Actions)="data">
 
-                    <button type="button" class="btn btn-secondary btn-circle"
-                        @click="switchStatus(data.item.id)"
-                        ><i class="ti-key"></i> </button>
-                    <button type="button" class="btn btn-secondary btn-circle"
-                        @click="switchStatus(data.item.id)"
-                        ><i class="ti-lock"></i> </button>
-                    <button type="button" class="btn btn-secondary btn-circle" 
-                        @click="goEdit(data.item.id)"
-                        ><i class="ti-pencil-alt"></i> 
-                    </button>
-                    <button type="button" class="btn btn-secondary btn-circle"  ><i class="ti-archive" ></i> </button>
+                    <vs-button @click="notifyArrival(data.item.id)" v-if="data.item.phase=='En Attend' && data.item.revente.etat=='Accepté'" color="primary" type="border" icon="meeting_room">
+                        Réçu
+                    </vs-button>
+                    <vs-button v-if="data.item.phase=='Reçu' && data.item.revente.etat=='Validé'"
+                        @click="sellProduct(data.item)" 
+                        color="success" type="border" icon="local_offer">
+                        Vendre
+                    </vs-button>
                     
                 </template>
                 </b-table>
+                <vs-popup title="Gérer votre revente" :active.sync="salingProduct">
+                        <div v-if="salingProduct" class="vs-list">
+                            <div class="vs-list--item">
+                                <div class="list-titles">
+                                    <div class="vs-list--title">Titre</div>
+                                    <div class="vs-list--subtitle"> {{ productToSale.name }} </div>
+                                </div>
+                                <div class="vs-list--slot"></div>
+                            </div>
+                            <div class="vs-list--item">
+                                <div class="list-titles">
+                                    <div class="vs-list--title">Acheté le :</div>
+                                    <div class="vs-list--subtitle"> {{ productToSale.createdAt | formatDate }} </div>
+                                </div>
+                                <div class="vs-list--slot"></div>
+                            </div>
+                            <div class="vs-list--item">
+                                <div class="list-titles">
+                                    <div class="vs-list--title">Couleur</div>
+                                    <div class="vs-list--subtitle"> {{ productToSale.couleur }} </div>
+                                </div>
+                                <div class="vs-list--slot"></div>
+                            </div>
+                            <div class="vs-list--item">
+                                <div class="list-titles">
+                                    <div class="vs-list--title">Age (mois)</div>
+                                    <div class="vs-list--subtitle"> {{ productToSale.age }} </div>
+                                </div>
+                                <div class="vs-list--slot"></div>
+                            </div>
+                            <div class="vs-list--item">
+                                <div class="list-titles">
+                                    <div class="vs-list--title">Prix de rachat</div>
+                                    <div class="vs-list--subtitle"> {{ productToSale.revente.prixPropose }} €  </div>
+                                </div>
+                                <div class="vs-list--slot"></div>
+                            </div>
+                            <div class="vs-list--item">
+                                <div class="list-titles">
+                                    <div class="vs-list--title">Modèle</div>
+                                    <div class="vs-list--subtitle"> {{ productToSale.modele.name }}  </div>
+                                </div>
+                                <div class="vs-list--slot"></div>
+                            </div>
+                        </div>
+                        <div class="default-input d-flex align-items-center mb-4">
+                            <div class="col-1"></div>
+                            <div class="vs-component vs-con-input-label vs-input inputx col-3 ml-4 vs-input-primary">
+                                <label for="" class="vs-input--label">prix de vente </label>
+                                <div class="vs-con-input">
+                                    <input class="vs-inputx vs-input--input normal" type="text" 
+                                        v-model="prixDeVente">
+                                    <code v-if="$v.prixDeVente.$error" >Champ requis</code>
+                                </div>
+                            </div>
+                            <div class="vs-component vs-con-input-label vs-input inputx col-3 ml-4 vs-input-primary">
+                                <label for="" class="vs-input--label">Titre </label>
+                                <div class="vs-con-input">
+                                    <input class="vs-inputx vs-input--input normal" type="text" 
+                                        v-model="nameDeVente">
+                                    <code v-if="$v.nameDeVente.$error" >Champ requis</code>
+                                </div>
+                            </div>
+                        </div>
+                        <div >
+                            <vs-button @click="putProductOnSale()" class="ml-5" color="rgb(3, 169, 243)" type="filled">
+                                Mettre en vente
+                            </vs-button>
+                        </div>
+                    
+                </vs-popup>
                 <div class="mt-3">
                     <b-pagination
                         v-model="c_Page"
@@ -93,6 +165,8 @@
 <script>
 import ThemifyIcon from "vue-themify-icons";
 import brc from '../custom/breadc.vue'
+import axios from 'axios';
+import { required, email, sameAs, minLength, requiredUnless, requiredIf } from 'vuelidate/lib/validators'
 /* eslint-disable */
 export default {
     components: {
@@ -101,12 +175,11 @@ export default {
     },
     data(){
         return{
-            fields: [ {label: "Nom complet", key :"name"  },
-                    {label: "Adresse mail", key: "email" },
-                    {label: "role", key: "role" },
-                    {label: "Créateur", key: "name" },
-                    {label: "Date de création", key: "created_at" },
-                    {label: "Status", key: "status" },
+            fields: [ {label: "Titre", key :"name"  },
+                    {label: "Modèle", key: "modele.name" },
+                    {label: "Marchand", key: "user.email" },
+                    {label: "Valeur (A)", key: "prixPropose" },
+                    {label: "Phase", key: "phase" },
                         "Actions"],
             c_Page: 1,
             p_Page: 5,
@@ -114,44 +187,85 @@ export default {
             tSearch: '',
             isBusy: false,
             tRows: 0,
-          
+            productToSale: null,
+            salingProduct: false,
+            prixDeVente: null, 
+            nameDeVente: null
         }
     },
     methods:{
         loadData(ctx, callback){
             console.log("loading")
-            /*
-            axios.get('/api/users'+"?page=" + this.c_Page + "&ppage=" + this.p_Page + "&srch=" + this.tSearch )
+            
+            axios.get('/products/all' )
             .then( result => {
                 console.log(result)
-                this.tRows = result.data.meta.total
-                callback(result.data.data)
+                this.tRows = result.data.Products.length
+                callback(result.data.Products)
                 this.isBusy = false
             }, error =>{
                 swal("Erreur!",
                         "Une erreur est survenue, veuillez contacter un administrateur",
                         "error");
             }  )
-            */
+            
         },
-        switchStatus(id){
-            console.log("switching status of user with id: ", id)
-            /*axios.put('api/users/switchstatus/' + id )
+        sellProduct(product){
+            this.salingProduct = true
+            this.productToSale = product
+            this.prixDeVente = (product.revente.prixPropose * 1.3).toFixed(0)
+            this.nameDeVente = product.modele.name
+            console.log( 'product :: ', product)
+        },
+        notifyArrival(id){
+            //product/notifyarrival/
+            axios.post('/product/notifyarrival/' + id )
                 .then(result =>{
-                        console.log(result)
+                        this.$refs.productsList.refresh();
+                        swal("Récéption enregistré",
+                                "Le marchand est notifié par mail, et sms.",
+                                "success");
                     },error => {
-                        console.log(error)
+                        swal("Erreur!",
+                                "Une erreur est survenue",
+                                "error");
                     }
                 )
-            this.$refs.userstab.refresh();*/
+            this.$refs.productsList.refresh();
         },
-        goEdit(id){
-            /*
-            this.$router.push('/user/edit/'+ id )*/
+        putProductOnSale(){
+            axios.post('/product/sale/' + this.productToSale.id , {
+                prix_vente : this.prixDeVente,
+                name : this.nameDeVente
+            } )
+                .then(result =>{
+                        this.$refs.productsList.refresh();
+                        this.salingProduct = false
+                        this.productToSale = null
+                        this.prixDeVente = null
+                        this.nameDeVente = null
+                        swal("Enregistré",
+                                "LeLe projet est mis en vente avec success.",
+                                "success");
+                    },error => {
+                        swal("Erreur!",
+                                "Une erreur est survenue",
+                                "error");
+                    }
+                )
+            this.$refs.productsList.refresh();
         },
         filter(){
             this.tSearch = this.search
             this.c_Page = 1
+        },
+    },
+    validations: {
+        prixDeVente: {
+          required,
+        },
+        nameDeVente: {
+          required,
         },
     }
 }
